b53c510233ef7df72f61b033e6275fb7fcea0382
Bigussyvert <135065315+Bigussyvert@users.noreply.github.com>
2023-10-18 14:46:29 -0400

    fixed the provider.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "bigussyvert-bucket"
     key            = "my-terraform-environment/main"
-    region         = "us-west-1"
+    region         = "us-east-1"
     dynamodb_table = "bigussyvert-table"
   }
 }
